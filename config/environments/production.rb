b27fea735ad0c01b3e57d80bd6c0719a0a54e739
Bro Sanje <bro.sanje@gmail.com>
2017-06-11 19:20:46 -0400

    email not working

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.delivery_method = :smtp
-  #config.action_mailer.default_url_options = { host: 'smtp.feastoffoo.xyz' }
+  config.action_mailer.default_url_options = { host: 'smtp.gmail.com' }
 
   ActionMailer::Base.smtp_settings = {
       :address        => 'smtp.gmail.com',
